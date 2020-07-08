58096f26d3d2cc37048e9db722caef9bca318060
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-07-08 00:18:53 -0700

    54. Renamed controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 module.exports = {
     auth: require('./authCtrl'),
     clips: require('./clipsCtrl'),
-    follow: require('./followCtrl'),
+    follow: require('./FollowCtrl'),
     replies: require('./repliesCtrl'),
     comments: require('./commentsCtrl'),
     accounts: require('./accountsCtrl'),
-    reports: require('./reportCtrl')
+    reports: require('./ReportCtrl')
 };
