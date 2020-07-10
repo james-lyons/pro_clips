15527df7e5f053dabaa89527b69219b6587926bd
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-07-10 11:08:12 -0700

    80. Testing mongodb uri

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const MONGO_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/pro-clips';
 
-console.log(process.env.MONGODB_URI)
+console.log('Hello from MONGO_URI', process.env.MONGODB_URI)
 
 // Connecting to MongoDB database
 
